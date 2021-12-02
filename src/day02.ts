2f03883dd69623a770f1a1f61dd46023941c9c13
Raf Ceuls <rceuls@gmail.com>
2021-12-02 22:19:17 +0100

    some stupid optimisation, part three

--- src/day02.ts
+++ src/day02.ts
@@ -4,62 +4,48 @@ type Position = {
 };
 
 type State = {
-  adjusted: {
-    horizontal: number;
-    vertical: number;
-    aim: number;
-  };
-  regular: {
-    horizontal: number;
-    vertical: number;
-  };
+  vertical: number;
+  verticalAdjusted: number;
+  horizontal: number;
+  aim: number;
 };
 
 const doMove = (state: State, move: string) => {
   switch (move[0]) {
     case "d": {
       const amount = +move.slice(5);
-      state.regular.vertical += amount;
-      state.adjusted.aim += amount;
+      state.vertical += amount;
+      state.aim += amount;
       break;
     }
     case "u":
       {
         const amount = +move.slice(3);
-        state.regular.vertical -= amount;
-        state.adjusted.aim -= amount;
+        state.vertical -= amount;
+        state.aim -= amount;
       }
       break;
     case "f": {
       const amount = +move.slice(7);
-      state.regular.horizontal += amount;
-      state.adjusted.horizontal += amount;
-      state.adjusted.vertical += amount * state.adjusted.aim;
+      state.horizontal += amount;
+      state.verticalAdjusted += amount * state.aim;
       break;
     }
   }
   return state;
 };
 
-const calc = (inbound: Position): number =>
-  inbound.horizontal * inbound.vertical;
-
 const calculate = (input: string[]) => {
   const finalPosition = input.reduce(doMove, {
-    adjusted: {
-      horizontal: 0,
-      vertical: 0,
-      aim: 0,
-    },
-    regular: {
-      horizontal: 0,
-      vertical: 0,
-    },
+    vertical: 0,
+    verticalAdjusted: 0,
+    aim: 0,
+    horizontal: 0,
   });
 
   return {
-    regular: calc(finalPosition.regular),
-    adjusted: calc(finalPosition.adjusted),
+    regular: finalPosition.vertical * finalPosition.horizontal,
+    adjusted: finalPosition.verticalAdjusted * finalPosition.horizontal,
   };
 };
 
