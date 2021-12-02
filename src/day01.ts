40d15a6440a13701dfc4e213a58d1a32d8fac11d
Raf Ceuls <rceuls@gmail.com>
2021-12-02 19:16:29 +0100

    refactoring day 2

--- src/day01.ts
+++ src/day01.ts
@@ -1,6 +1,28 @@
 const calculate: (input: number[]) => number = (data) =>
   data.filter((_, i, arr) => arr[i - 1] < arr[i]).length;
 
+const addRelevantItems = (input: number[]) => {
+  let currCount = 0;
+  let currItem = 0;
+  const grouped: number[] = [];
+
+  for (let i = 0; i < input.length; i++) {
+    currItem += input[i];
+    if (currCount === 3) {
+      grouped.push(currItem);
+      currCount = 0;
+      currItem = 0;
+    }
+  }
+  if (currCount !== 0) {
+    grouped.push(currItem);
+  }
+  return grouped;
+};
+
+const calculatePartTwoo: (input: number[]) => number = (input) =>
+  calculate(addRelevantItems(input));
+
 const sumRelevantItems: (input: number[], index: number) => number = (
   input,
   index
