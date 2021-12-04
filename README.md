e30edae400807678fdbef7ca9dab94366c730e1b
Raf Ceuls <rceuls@gmail.com>
2021-12-04 17:55:05 +0100

    generic refactoring

--- README.md
+++ README.md
@@ -3,3 +3,21 @@
 ## What?
 
 - Advent of code solutions, written in typescript for usage on Deno.
+
+## How?
+
+### Binary
+
+`vr build && ./entrypoint | gnomon`
+
+### Tests
+
+`deno test --watch --allow-all`
+
+### Profiler
+
+1. `deno run --inspect-brk --allow-all ./entrypoint.ts`
+2. `chrome://inspect`
+3. start profiling
+4. stop profiling
+5. quit process.
--- entrypoint.ts
+++ entrypoint.ts
@@ -1,3 +1,5 @@
+import { parse } from "https://deno.land/std@0.117.0/flags/mod.ts";
+
 import * as Day01 from "./src/2021/day01.ts";
 import * as Day02 from "./src/2021/day02.ts";
 import * as Day03 from "./src/2021/day03.ts";
@@ -14,16 +16,45 @@ const part02 = getLines("day02.input.txt", 2021);
 const part03 = getLines("day03.input.txt", 2021);
 const part04 = getText("day04.input.txt", 2021);
 
-console.log("start");
+const runAll = () => {
+  console.log("start");
 
-console.log(`Day 01 > Part 01 > ${Day01.calculateDay01Part01(part01)}`);
-console.log(`Day 01 > Part 02 > ${Day01.calculateDay01Part02(part01)}`);
+  for (let x = 1; x < 5; x++) {
+    runSingle(x);
+  }
 
-console.log(`Day 02 > Part 01 + 02 > ${Day02.calculateDay02(part02)}`);
+  console.log("end");
+};
 
-console.log(`Day 03 > Part 01 > ${Day03.calculateDay03Part01(part03)}`);
-console.log(`Day 03 > Part 02 > ${Day03.calculateDay03Part02(part03)}`);
+const runSingle = (targetDay: number) => {
+  switch (targetDay) {
+    case 1:
+      console.log(`Day 01 > Part 01 > ${Day01.calculateDay01Part01(part01)}`);
+      console.log(`Day 01 > Part 02 > ${Day01.calculateDay01Part02(part01)}`);
+      break;
+    case 2:
+      console.log(`Day 02 > Part 01 + 02 > ${Day02.calculateDay02(part02)}`);
+      break;
+    case 3:
+      console.log(`Day 03 > Part 01 > ${Day03.calculateDay03Part01(part03)}`);
+      console.log(`Day 03 > Part 02 > ${Day03.calculateDay03Part02(part03)}`);
+      break;
+    case 4:
+      console.log(`Day 04 > Part 01 + 02 > ${Day04.calculateDay04(part04)}`);
+      break;
+    default:
+      throw new Error("You forgot something");
+  }
+};
 
-console.log(`Day 04 > Part 01 + 02 > ${Day04.calculateDay04(part04)}`);
+const args = parse(Deno.args);
+const targetDay = args._.length === 0 ? 0 : +args._[0];
 
-console.log("end");
+if (targetDay === 0) {
+  console.log("Running all days");
+  runAll();
+} else {
+  console.log(`Running only day ${targetDay}`);
+  runSingle(targetDay);
+}
+console.log("done");
