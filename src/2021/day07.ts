7255ea23a425784270602226927f009b3776cfb3
Raf Ceuls <rceuls@gmail.com>
2021-12-07 19:05:27 +0100

    day07 - more optimal version

--- src/2021/day07.ts
+++ src/2021/day07.ts
@@ -1,35 +1,38 @@
-const calculate = (input: string) => {
-  const initialState = input.split(",").map((x) => +x);
-  initialState.sort((x, y) => x - y);
+const parseInput = (input: string) =>
+  input
+    .split(",")
+    .map((x) => +x)
+    .sort((x, y) => x - y);
 
+const moveCost = (m: number) => (m * (m + 1)) / 2;
+
+const calculate = (input: string) => {
+  const initialState = parseInput(input);
   const first = initialState.slice(0, initialState.length / 2);
   const last = initialState.slice(initialState.length / 2);
-  return last.map((x, i) => x - first[i]).reduce((p, n) => p + n);
-};
 
-const moveCost = (num: number) => {
-  let result = 0;
-  for (let i = 1; i <= num; i++) {
-    result += i;
-  }
-  return result;
+  return last.reduce((p, n) => p + n) - first.reduce((p, n) => p + n);
 };
 
 const calculatePartTwo = (input: string) => {
-  const initialState = input.split(",").map((x) => +x);
-  initialState.sort((x, y) => x - y);
+  const initialState = parseInput(input);
+
   const min = initialState[0];
   const max = initialState[initialState.length - 1];
 
-  const totals = [];
-
+  let minFuel = Infinity;
   for (let i = min; i < max; i++) {
-    totals.push(
-      initialState.reduce((p, n) => moveCost(Math.abs(n - i)) + p, 0)
+    const fuel = initialState.reduce(
+      (p, n) => moveCost(Math.abs(n - i)) + p,
+      0
     );
+
+    if (fuel < minFuel) {
+      minFuel = fuel;
+    }
   }
 
-  return totals.sort((x, y) => x - y)[0];
+  return minFuel;
 };
 
 export {
