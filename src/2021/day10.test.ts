e9a4afb4155caccde3959b7c1a62fd0b6b242566
Raf Ceuls <rceuls@gmail.com>
2021-12-10 13:00:03 +0100

    day 10

--- src/2021/day10.test.ts
+++ src/2021/day10.test.ts
@@ -1,14 +1,14 @@
 import { assertEquals } from "https://deno.land/std@0.116.0/testing/asserts.ts";
 import { calculateDay10Part01, calculateDay10Part02 } from "./day10.ts";
-import { getText } from "../shared/file-util.ts";
+import { getLines } from "../shared/file-util.ts";
 import { TEST_DEFAULTS } from "../shared/test-util.ts";
 
 Deno.test({
   name: "2021 > day 10 > part 1 > example",
   fn() {
     assertEquals(
-      calculateDay10Part01(getText("day10.test.input.txt", 2021)),
-      1
+      calculateDay10Part01(getLines("day10.test.input.txt", 2021)).points,
+      26397
     );
   },
   ...TEST_DEFAULTS,
@@ -18,9 +18,17 @@ Deno.test({
   name: "2021 > day 10 > part 2 > example",
   fn() {
     assertEquals(
-      calculateDay10Part01(getText("day10.test.input.txt", 2021)),
-      1
+      calculateDay10Part02(getLines("day10.test.input.txt", 2021)),
+      288957
     );
+  },
+  ...TEST_DEFAULTS,
+});
+
+Deno.test({
+  name: "2021 > day 10 > part 2 > example smol",
+  fn() {
+    assertEquals(calculateDay10Part02(["<{([{{}}[<[[[<>{}]]]>[]]"]), 294);
   },
   ...TEST_DEFAULTS,
 });
@@ -28,8 +36,8 @@ Deno.test({
 Deno.test({
   name: "2021 > day 10 > part 1 > actual",
   fn() {
-    const input = getText("day10.input.txt", 2021);
-    assertEquals(calculateDay10Part02(input), 1);
+    const input = getLines("day10.input.txt", 2021);
+    assertEquals(calculateDay10Part01(input).points, 296535);
   },
   ...TEST_DEFAULTS,
 });
@@ -37,8 +45,8 @@ Deno.test({
 Deno.test({
   name: "2021 > day 10 > part 2 > actual",
   fn() {
-    const input = getText("day10.input.txt", 2021);
-    assertEquals(calculateDay10Part02(input), 1);
+    const input = getLines("day10.input.txt", 2021);
+    assertEquals(calculateDay10Part02(input), 4245130838);
   },
   ...TEST_DEFAULTS,
 });
--- src/2021/day10.ts
+++ src/2021/day10.ts
@@ -1,6 +1,60 @@
-const calculate01 = (_input: string) => 1;
+const starters = "([{<".split("");
+const stoppers = ")]}>".split("");
+const misMatchPoints = [3, 57, 1197, 25137];
+const addingPoints = [1, 2, 3, 4];
 
-const calculate02 = (_input: string) => 1;
+const cleanLine = (line: string[]) => {
+  for (let i = 0; i < line.length - 1; ) {
+    const fi = starters.indexOf(line[i]);
+    const si = stoppers.indexOf(line[i + 1]);
+    if (fi > -1 && si > -1) {
+      if (fi === si) {
+        line.splice(i, 2);
+        i = 0;
+        continue;
+      }
+    }
+    i++;
+  }
+  return line;
+};
+
+const calculate01 = (input: string[]) => {
+  const pts = [];
+  const corruptLines = [];
+  const lines = input.map((x) => x.split(""));
+  let lineIndex = 0;
+  for (const origin of lines) {
+    const line = cleanLine(origin);
+    const firstCloser = line.findIndex((x) => stoppers.includes(x));
+    if (firstCloser > -1) {
+      pts.push(misMatchPoints[stoppers.indexOf(line[firstCloser])]);
+      corruptLines.push(lineIndex);
+    }
+    lineIndex++;
+  }
+  return { points: pts.reduce((n, p) => n + p, 0), corruptLines };
+};
+
+const calculate02 = (input: string[]) => {
+  const corruptedLines = calculate01(input).corruptLines;
+  const lines = input.map((x) => x.split(""));
+  const scores: number[] = [];
+  for (let i = 0; i < lines.length; i++) {
+    if (!corruptedLines.includes(i)) {
+      const line = cleanLine(lines[i]).reverse();
+      let val = 0;
+      for (let j = 0; j < line.length; j++) {
+        const ix = starters.indexOf(line[j]);
+        const toAdd = addingPoints[ix];
+        val *= 5;
+        val += toAdd;
+      }
+      scores.push(val);
+    }
+  }
+  return scores.sort((x, y) => x - y)[Math.floor(scores.length / 2)];
+};
 
 export {
   calculate01 as calculateDay10Part01,
